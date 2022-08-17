fd4c27b9d3d84b1f95880d9f47d60e470e4e7011
Taha-Alkalfot <111251639+Taha-Alkalfot@users.noreply.github.com>
2022-08-18 00:11:41 +0300

    Add files via upload

---
+++ src/Q4.java
@@ -0,0 +1,8 @@
+public class Q4 {
+    public static void main(String[] args) {
+        System.out.println((1000*0.05)*3+1000);
+
+
+
+    }
+}
---
+++ src/Q5.java
@@ -0,0 +1,8 @@
+public class Q5 {
+    public static void main(String[] args) {
+        System.out.println("___________________");
+        System.out.println("|  Taha Alklfot   |");
+        System.out.println("___________________");
+
+    }
+}
